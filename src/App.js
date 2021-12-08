a23ba2845fb084d7c68fa38258a4722270dd0bfa
uddeshya kumar <uddeshyak61@gmail.com>
2021-12-08 04:14:09 -0800

    Basic Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Home from "./components/Tabs/Home.js"
 import './App.css';
+import {Tabs, Tab, AppBar} from '@material-ui/core';
+
 
 function App() {
+  const [value, setValue]=React.useState(0);
+  const handleTabs=(e,val)=>{
+    console.warn(val);
+    setValue(val);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AppBar>
+          <Tabs value={value} onChange={handleTabs}>
+            <Tab label="Home" />
+            <Tab label="Projects" />
+            <Tab label="Contact" />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}><Home/></TabPanel>
+        <TabPanel value={value} index={1}>Projects</TabPanel>
+        <TabPanel value={value} index={2}>Contact</TabPanel>
     </div>
   );
 }
 
+function TabPanel(props){
+  const {children, value, index}=props;
+  return(
+    <div>
+      {
+        value===index && (<h1>{children}</h1>)
+      }
+    </div>
+  )
+}
 export default App;
---
+++ src/components/Tabs/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Home.css';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import DevicesIcon from '@mui/icons-material/Devices';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
+function Home() {
+        return(
+        <div>
+            <div className="name">
+                Hello, I'm Uddeshya Kumar
+                <p>I'm a Full-Stack Software Engineer</p>
+            </div>
+             <div className="icons">
+                <div>
+                <DeleteIcon color="primary" sx={{fontSize:100}}/>
+                <h7>Trash</h7>
+                <p>Trash how is this happening</p>
+                </div>
+                <div>
+                <RocketLaunchIcon color="primary" sx={{fontSize:100}}/>
+                <h7>Fast</h7>
+                <p>I'm fast as fuck boiiiii</p>
+                </div>
+                <div>
+                <DevicesIcon color="primary" sx={{fontSize:100}}/>
+                <h7>Smart</h7>
+                <p>It's big brain time</p>
+                </div>
+                <div>
+                <LightbulbIcon color="primary" sx={{fontSize:100}}/>
+                <h7>Innovative</h7>
+                <p>Create something new out of nothing</p>
+                </div>
+            </div>
+            <div className='intro'>
+                    <img className='image' src='/Profile2.jpg' alt='' width="200" height="200"/>
+            </div>
+            <div className='summary'>
+            </div>
+            <div className='skills'>
+            </div>
+        </div>
+        );
+}
+
+export default Home;
